0699715feeb4e47bc3fd19768eb0baf6de980f33
Siemen Sikkema <siemen@unclutter.nl>
2017-05-22 16:28:39 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporValidation",
     dependencies: [
-        .Package(url: "https://github.com/vapor/validation.git", majorVersion: 0),
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
