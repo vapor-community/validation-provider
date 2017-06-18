fd42c5ab63436eda9e7ee03a0492eaca71e445af
Siemen Sikkema <siemen@unclutter.nl>
2017-06-18 18:20:24 +0100

    Rename sources directory

---
+++ Sources/ValidationProvider/Provider.swift
@@ -0,0 +1,20 @@
+import Vapor
+@_exported import Validation
+
+public final class Provider: Vapor.Provider {
+    public static let repositoryName = "validation-provider"
+    
+    public init() {}
+    public init(config: Config) {}
+
+    public func boot(_ config: Config) throws {
+        config.addConfigurable(
+            middleware: ValidationMiddleware.init,
+            name: "validation"
+        )
+    }
+    
+    public func boot(_ drop: Droplet) throws {}
+
+    public func beforeRun(_ drop: Droplet) throws {}
+}
